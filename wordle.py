a0fa01fa7b3b1468253ec274bfc3554d0958ba67
SteveBenz <stbenz@microsoft.com>
2022-05-26 11:03:36 -0700

    Added a mode for assisting in playing

--- wordle.py
+++ wordle.py
@@ -1,71 +1,113 @@
 # Read the answers and allowed file.
-from asyncio.windows_events import NULL
-from asyncore import read
 from distutils.log import error
 import random
 import sys
 import wordle_module
 import argparse
+import re
+
+def play(target: str or None):
+    f = open("answers.txt", "r")
+    answers = []
+    l = f.readline()
+    while l:
+        if l != "\n":
+            answers.append(l.rstrip())
+        l = f.readline()
+    f.close()
+
+    f = open("allowed.txt", "r")
+    allowed = []
+    l = f.readline()
+    while l:
+        if l != "\n":
+            allowed.append(l.rstrip())
+        l = f.readline()
+    f.close()
+
+    if target:
+        if target not in answers:
+            error(f"Target word given, {target}, is not in the list of known words")
+            exit(1)
+    else:
+        target = answers[random.randrange(len(answers))]
+
+    possibleAnswers = answers
+    print(f"Starting with {len(possibleAnswers)} possible answers.")
+    print("1> ", end="", flush=True)
+    guess = sys.stdin.readline().strip()
+    guesses = 1
+    while guess != target:
+        if guess == "?":
+            print("Magic words:")
+            print(" show - shows the word you're after")
+            print(" list - shows the list of words that could match")
+            print(" suggest - shows the best word to use")
+            print("")
+        elif guess == 'show':
+            print(f'Selected word is {target}')
+        elif guess == 'list':
+            print(" ".join(possibleAnswers))
+        elif guess == 'suggest':
+            wordle_module.suggest(possibleAnswers, allowed, 5)
+        elif guess in allowed or guess in answers:
+            hint = wordle_module.checkAnswer(guess,target)
+            guesses += 1
+            possibleAnswers = list(filter(lambda suspect: not wordle_module.isEliminated(suspect, guess, hint), possibleAnswers))
+            if target not in possibleAnswers:
+                error(f"Whoopsadoodle - actual target of {target} was eliminated") # Must be a bug in isEliminated
+                exit(2)
+            print(f"   {hint}    {len(possibleAnswers)} possibilities remain")
+        else:
+            print("Sorry, that's not a valid word")
+        print(f'{guesses}> ', end="", flush=True)
+        guess = sys.stdin.readline().strip()
+
+    print(f"Won in {guesses} tries")
+
+def assist():
+    f = open("answers.txt", "r")
+    answers = []
+    l = f.readline()
+    while l:
+        if l != "\n":
+            answers.append(l.rstrip())
+        l = f.readline()
+    f.close()
+
+    possibleAnswers = answers
+    print(f"Starting with {len(possibleAnswers)} possible answers.")
+    print("Magic words:")
+    print(" list - shows the list of words that could match")
+    print("")
+    while True:
+        guess: str = "*"
+        while guess != "list" and not re.match("^[a-z]{5}$", guess):
+            if guess != "*":
+                print("Guess must be 5 letters long")
+            print("Guess> ", end="", flush=True)
+            guess = sys.stdin.readline().strip()
+
+        if guess == 'list':
+            print(" ".join(possibleAnswers))
+        else:
+            hint = "*"
+            while not re.match("^[ =^]{0,5}$", hint):
+                print(" Hint> ", end="", flush=True)
+                hint = sys.stdin.readline().strip("\r\n")
+            while len(hint) < 5:
+                hint += " "
+
+            possibleAnswers = list(filter(lambda suspect: not wordle_module.isEliminated(suspect, guess, hint), possibleAnswers))
+            print(f"{len(possibleAnswers)} possible answers remain.")
 
 argumentParser = argparse.ArgumentParser(description="Wordle back seat quarterback")
 argumentParser.add_argument("-w", "--word", help="If supplied, plays the wordle game with the target being the word given.")
+argumentParser.add_argument("-a", "--assist", action='store_true',
+    help="If supplied, tells it to go into a mode where you supply your guesses and the game's response so that you can get it to list the words")
 arguments = argumentParser.parse_args()
-target = arguments.word
-
-f = open("answers.txt", "r")
-answers = []
-l = f.readline()
-while l:
-    if l != "\n":
-        answers.append(l.rstrip())
-    l = f.readline()
-f.close()
-
-f = open("allowed.txt", "r")
-allowed = []
-l = f.readline()
-while l:
-    if l != "\n":
-        allowed.append(l.rstrip())
-    l = f.readline()
-f.close()
-
-if target:
-    if target not in answers:
-        error(f"Target word given, {target}, is not in the list of known words")
-        exit(1)
+if (arguments.assist):
+    assist()
 else:
-    target = answers[random.randrange(len(answers))]
+    play(arguments.word)
 
-possibleAnswers = answers
-print(f"Starting with {len(possibleAnswers)} possible answers.")
-print("1> ", end="", flush=True)
-guess = sys.stdin.readline().strip()
-guesses = 1
-while guess != target:
-    if guess == "?":
-        print("Magic words:")
-        print(" show - shows the word you're after")
-        print(" list - shows the list of words that could match")
-        print(" suggest - shows the best word to use")
-        print("")
-    elif guess == 'show':
-        print(f'Selected word is {target}')
-    elif guess == 'list':
-        print(" ".join(possibleAnswers))
-    elif guess == 'suggest':
-        wordle_module.suggest(possibleAnswers, allowed, 5)
-    elif guess in allowed or guess in answers:
-        hint = wordle_module.checkAnswer(guess,target)
-        guesses += 1
-        possibleAnswers = list(filter(lambda suspect: not wordle_module.isEliminated(suspect, guess, hint), possibleAnswers))
-        if target not in possibleAnswers:
-            error(f"Whoopsadoodle - actual target of {target} was eliminated") # Must be a bug in isEliminated
-            exit(2)
-        print(f"   {hint}    {len(possibleAnswers)} possibilities remain")
-    else:
-        print("Sorry, that's not a valid word")
-    print(f'{guesses}> ', end="", flush=True)
-    guess = sys.stdin.readline().strip()
-
-print(f"Won in {guesses} tries")
